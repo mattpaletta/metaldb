d8c4c6affbb319ab79ae309072f1c607dccc6cb1
Matthew Paletta <mattpaletta@gmail.com>
2022-04-23 10:52:33 +0100

    readd filter instruction

--- libs/engine_shaders/engine.h
+++ libs/engine_shaders/engine.h
@@ -5,7 +5,7 @@
 #include "instruction_type.h"
 #include "parse_row_instruction.h"
 #include "projection_instruction.h"
-//#include "filter_instruction.h"
+#include "filter_instruction.h"
 #include "output_instruction.h"
 #include "method.h"
 #include "temp_row.h"
